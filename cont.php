78a8dfdaf646b5cdd40185d33ddece971bc083f9
Raresssm <lungurares4@gmail.com>
2024-01-08 14:10:22 +0200

    a

--- cont.php
+++ cont.php
@@ -1,19 +1,18 @@
 <?php
+    include("database.php");
+?>
+
+<?php
 $username = isset($_POST["user"]) ? $_POST["user"] : null;
 $password = isset($_POST["pass"]) ? $_POST["pass"] : null;
-$db = mysqli_connect("localhost", "Rares1", "Password_12", "cafenea");
-if (!$db) {
- exit('Conectare esuata: ' . mysqli_connect_error ());
-}
-echo 'Conectare reusita';
 $interogare = "INSERT INTO cont (username, password)
  VALUES ('$username' , '$password')";
-mysqli_query($db,$interogare);
+mysqli_query($conn,$interogare);
 
-if (mysqli_errno($db))
- exit('<br>Adaugare esuata: '.mysqli_errno($db).": ".mysqli_error($db)."<BR>");
+if (mysqli_errno($conn))
+ exit('<br>Adaugare esuata: '.mysqli_errno($conn).": ".mysqli_error($conn)."<BR>");
 
-echo "<br> Conectare reusita!";
+echo "Conectare reusita!";
 
-mysqli_close($db);
+mysqli_close($conn);
 ?> 
--- database.php
+++ database.php
@@ -1,14 +1,20 @@
 <?php 
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
 	$db_server = "localhost";
     $db_user = "Rares1";
     $db_pass = "Password_12";
-    $db_name = "espresso_zarnesti";
-    $conn = "";
+    $db_name = "cafenea";
 
 
-    $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
-    if(!$conn){
-        exit('Conectare esuata: ' . mysqli_connect_error ());
+  //  $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
+    try {
+        // Attempt to establish a database connection
+        $conn = mysqli_connect($db_server, $db_user, $db_pass, $db_name);
+    
+        // Continue with other code if the connection is successful
+    
+    } catch (mysqli_sql_exception $e) {
+        exit ("Conectare esuata!");
     }
-    echo "You are connected!";
 ?>
